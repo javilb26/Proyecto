50a09c97c561b52372d48bd35d4ea42a5d4b1ab6
Javier Losa Balsa <javier.losa.balsa@udc.es>
2016-03-04 21:59:15 +0100

    Añadido al controlador modificar estado actual

--- src/main/java/com/journaldev/spring/TaxiController.java
+++ src/main/java/com/journaldev/spring/TaxiController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,62 +29,53 @@ public class TaxiController {
 		this.taxiService = taxiService;
 	}
 	
-	@RequestMapping(value = "/taxis", method = RequestMethod.GET)
-	public String getTaxis(Model model) {
-		model.addAttribute("taxi", new Taxi());
-		model.addAttribute("getTaxis", this.taxiService.getTaxis());
-		return "taxi";
-	}
-	
-	@RequestMapping(value= "/taxi/add", method = RequestMethod.POST)
-	public String createTaxi(@ModelAttribute("taxi") Taxi taxi) throws InstanceNotFoundException, IncorrectPasswordException{
-		if(taxi.getTaxiId() == 0){
-			if (taxi.getActualState()==null) {
-				taxi.setActualState(State.OFF);
-			}
-			if (taxi.getFutureState()==null) {
-				taxi.setFutureState(State.OFF);
-			}
-			this.taxiService.createTaxi(taxi);
-		}else{
-			this.taxiService.updateActualStateTaxi(taxi.getTaxiId(), taxi.getActualState());
-			this.taxiService.updateFutureStateTaxi(taxi.getTaxiId(), taxi.getFutureState());
+//	@RequestMapping(value = "/taxis", method = RequestMethod.GET)
+//	public String getTaxis(Model model) {
+//		model.addAttribute("taxi", new Taxi());
+//		model.addAttribute("getTaxis", this.taxiService.getTaxis());
+//		return "taxi";
+//	}
+//	
+//	@RequestMapping(value= "/taxi/add", method = RequestMethod.POST)
+//	public String createTaxi(@ModelAttribute("taxi") Taxi taxi) throws InstanceNotFoundException, IncorrectPasswordException{
+//		if(taxi.getTaxiId() == 0){
+//			if (taxi.getActualState()==null) {
+//				taxi.setActualState(State.OFF);
+//			}
+//			if (taxi.getFutureState()==null) {
+//				taxi.setFutureState(State.OFF);
+//			}
+//			this.taxiService.createTaxi(taxi);
+//		}else{
+//			this.taxiService.updateActualStateTaxi(taxi.getTaxiId(), taxi.getActualState());
+//			this.taxiService.updateFutureStateTaxi(taxi.getTaxiId(), taxi.getFutureState());
 //			this.taxiService.changePassword(taxi.getTaxiId(), this.taxiService.getTaxiById(taxi.getTaxiId()).getPassword(), taxi.getPassword());
-		}
-		return "redirect:/taxis";
-	}
-	
-	@RequestMapping("/remove/{id}")
-    public String removeTaxi(@PathVariable("id") Long id) throws InstanceNotFoundException{
-        this.taxiService.removeTaxi(id);
-        return "redirect:/taxis";
-    }
- 
-    @RequestMapping("/edit/{id}")
-    public String editTaxi(@PathVariable("id") Long id, Model model) throws InstanceNotFoundException{
-        model.addAttribute("taxi", this.taxiService.getTaxiById(id));
-        model.addAttribute("getTaxis", this.taxiService.getTaxis());
-        return "taxi";
-    }
-	
-//    @RequestMapping("/login")
-//    public String loginTaxi(@ModelAttribute("taxi") Taxi taxi) throws InstanceNotFoundException, IncorrectPasswordException{
-//    	boolean passwordIsEncrypted = false;
-//        this.taxiService.login(taxi.getTaxiId(), taxi.getPassword(), passwordIsEncrypted);
+//		}
+//		return "redirect:/taxis";
+//	}
+//	
+//	@RequestMapping("/remove/{id}")
+//    public String removeTaxi(@PathVariable("id") Long id) throws InstanceNotFoundException{
+//        this.taxiService.removeTaxi(id);
+//        return "redirect:/taxis";
+//    }
+// 
+//    @RequestMapping("/edit/{id}")
+//    public String editTaxi(@PathVariable("id") Long id, Model model) throws InstanceNotFoundException{
+//        model.addAttribute("taxi", this.taxiService.getTaxiById(id));
+//        model.addAttribute("getTaxis", this.taxiService.getTaxis());
 //        return "taxi";
 //    }
     
-    @RequestMapping(value = "/login/{taxiId}/{password}", method = RequestMethod.GET, produces = "application/json")
-    public Taxi loginTaxi(@PathVariable Long taxiId, @PathVariable String password) throws InstanceNotFoundException, IncorrectPasswordException{
+    @RequestMapping(value = "/login", method = RequestMethod.POST, produces = "application/json")
+    public Taxi login(@RequestBody Taxi input) throws InstanceNotFoundException, IncorrectPasswordException{
 		boolean passwordIsEncrypted = false;
-		taxi = this.taxiService.login(taxiId, password, passwordIsEncrypted);
+		taxi = this.taxiService.login(input.getTaxiId(), input.getPassword(), passwordIsEncrypted);
       	return taxi;
     }
     
-    @RequestMapping("/password")
-    public String changePasswordTaxi(@ModelAttribute("taxi") Taxi taxi) throws InstanceNotFoundException, IncorrectPasswordException{
-        this.taxiService.changePassword(taxi.getTaxiId(), this.taxiService.getTaxiById(taxi.getTaxiId()).getPassword(), taxi.getPassword());
-        return "taxi";
+    @RequestMapping(value = "/taxi/{taxiId}/state/{state}", method = RequestMethod.PUT, produces = "application/json")
+    public void updateActualStateTaxi(@PathVariable("taxiId") Long taxiId, @PathVariable("state") String state) throws InstanceNotFoundException {
+    	this.taxiService.updateActualStateTaxi(taxiId, State.valueOf(state.toUpperCase()));
     }
-    
 }
